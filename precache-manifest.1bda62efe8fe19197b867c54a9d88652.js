9e5e3c451943f2f94415afd2c5fe4a712fca04e7
David Piper <black.god1930@gmail.com>
2019-08-22 07:34:57 -0400

    Updates

---
+++ precache-manifest.1bda62efe8fe19197b867c54a9d88652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a765860695619b6a6dbfee4c1f0851",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "c3a7cdf82c58a75b5b4a",
+    "url": "/burger-builder/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e9f335e19a36a47dfc28",
+    "url": "/burger-builder/static/js/2.3e2d954d.chunk.js"
+  },
+  {
+    "revision": "c3a7cdf82c58a75b5b4a",
+    "url": "/burger-builder/static/js/main.8012e482.chunk.js"
+  },
+  {
+    "revision": "cf0ef0340420e745270e",
+    "url": "/burger-builder/static/js/runtime~main.b587fc36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burger-builder/static/media/logo.5d5d9eef.svg"
+  }
+]);
